bb982667e8f2271ab3be4d7acc5d43544fc39bbe
Leonard Tan <askleonard@gmail.com>
2014-09-20 12:46:45 -0700

    Submit Assignment 2 by Leonard

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,46 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m<-NULL
+    set<-function(y){
+        x<<-y
+        m<<-NULL
+    }
+    get<-function() x
+    setmatrix<-function(solve) m<<- solve
+    getmatrix<-function() m
+    list(set=set, get=get,
+         setmatrix=setmatrix,
+         getmatrix=getmatrix)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+## Return a matrix that is the inverse of 'x'
+    m<-x$getmatrix()
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    matrix<-x$get()
+    m<-solve(matrix, ...)
+    x$setmatrix(m)
+    m
+}
+
+cacheTest <- function()
+{
+    Y <- matrix(c(3,3.2,3.5,3.6),2,2)
+    cache<-makeCacheMatrix()
+    cache$set(Y)
+    iY <- cacheSolve(cache)
+    
+## The inverse of Y is Y^-1 only when:
+## Y x Y^-1 = I
+
+    ans <- Y %*% iY
+    ans
+    
 }
